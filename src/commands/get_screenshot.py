c12d362a2f7cd573c337d5e40deefb89ec2d8194
Ulansyn Toktorbai uulu <100096643+ulansyn@users.noreply.github.com>
2024-05-10 04:17:35 +0600

    Delete src/commands directory

--- src/commands/get_screenshot.py
+++
@@ -1,6 +0,0 @@
-from PIL import ImageGrab
-
-
-def get_screenshot():
-    screenshot = ImageGrab.grab()
-    screenshot.save("PATH")
--- src/commands/lock_screen.py
+++
@@ -1,6 +0,0 @@
-import ctypes
-
-
-def lock_screen():
-    user32 = ctypes.windll.User32
-    user32.LockWorkStation()
